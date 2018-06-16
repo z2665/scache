c1fa37bdf9ed165809b36496bb37db48366fff6e
z2665 <z26650@hotmail.com>
2018-06-16 20:14:33 +0800

    加入测试

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,26 @@
 
 #include <iostream>
 #include<string>
+#include<ctime>
 #include"cache.h"
+
 
 using namespace std;
 
 // TDOO: emmmmm
 int main(int argc, char const *argv[])
-{   
+{    
+    clock_t start,finish;
     cout<<"hello"<<endl;
     cout<<"world"<<endl;
     Scache::cache c(2000);
-    c.set(string("hello"), string("world"));
+    start=clock();
+    //简单的重复插入测试
+    for (int i = 0; i != 100000;++i){
+        c.set(string("hello"), string("world"));
+    }     
+    finish=clock();
+    
+    cout << "\n简单的重复插入运行时间为" << (double)(finish - start) / CLOCKS_PER_SEC << endl;
     return 0;
 }
