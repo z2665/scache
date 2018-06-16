55eb9c2ff9c272165d96e381361c42cd93cd06b2
z2665 <z26650@hotmail.com>
2018-06-16 16:18:45 +0800

    完成set部分

--- src/cache.h
+++ src/cache.h
@@ -1,10 +1,12 @@
 #pragma once
 #include<string>
+namespace Scache{
+
 
 class item
 {
     public:
-        item(std::string key, std::string  value): _key(std::move(key)), _value(std::move(value)),next(nullptr) {
+       item(std::string key, std::string  value): _key(std::move(key)), _value(std::move(value)),next(nullptr) {
         }
         void SetNext(item *v){
             next = v;
@@ -12,7 +14,7 @@ class item
         item* Next(){
             return next;
         }
-        std::string& Key(){
+        std::string Key(){
             return _key;
         }
         std::string Value(){
@@ -31,14 +33,72 @@ class cache
 private:
     //hash数组总容量
   int cap;
-  item* hashmap[];
+  int depth;
+  bool ishasing;
+  item** hashmap;
   
 public:
-    cache(int size=200):cap(size){
+    cache(int size=200,int dep=6):cap(size),depth(dep),ishasing(false){
         hashmap = new item*[size];
     };
     ~cache(){
         
     };
+    //hash函数，后续更换为实现更优秀版本
+    int hash(std::string& key){
+        int tmp=0;
+        for (auto& ch : key){
+            tmp += ch&0xf00000;
+        }
+        return tmp%cap;
+    }
+    //放大hashmap大小并重新hash
+    void rehash(){
+        int oldcap = cap;
+        cap *= 2;
+        item** newmap = new item *[cap];
+        for (int i = 0; i != oldcap;++i){
+            item *oldone = hashmap[i];
+            //如果当前位置不为空，则遍历链表并重新hash所有的项
+            while(oldone!=nullptr){
+                set(oldone->Key(), oldone->Value());
+                oldone = oldone->Next();
+            }
+        }
+        ishasing = false;
+        delete[] hashmap;
+        hashmap = newmap;
+    }
+    void set(std::string key, std::string value){
+        int p=hash(key);
+        item * tmp=new item(key,value);
+        //如果这个位置没有被占用，则直接插入
+        rehashpoint:
+        if (hashmap[p]==nullptr){
+            hashmap[p]=tmp;
+           return; 
+        }
+        //否则进入冲突处理逻辑
+        item* next=hashmap[p];
+        int len = 0;
+        while (true){
+            //如果链尾不为空，则插入
+            if (next->Next()==nullptr ){
+                next->SetNext(tmp);
+                return;
+            }
+            next = next->Next();
+            len++;
+            if (len>=depth && !ishasing){
+                //如果链表深度大于限制
+                ishasing = true;
+                rehash();
+                len = 0;
+                goto rehashpoint;
+            }
+        }
+    }
 };
+
+}
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include<string>
+#include"cache.h"
 
 using namespace std;
 
@@ -9,5 +10,7 @@ int main(int argc, char const *argv[])
 {   
     cout<<"hello"<<endl;
     cout<<"world"<<endl;
+    Scache::cache c(2000);
+    c.set(string("hello"), string("world"));
     return 0;
 }
