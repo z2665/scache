c87d79b1f64bdde02a8164188c38fc9e927c9461
z2665 <z26650@hotmail.com>
2018-07-10 23:32:03 +0800

    fix error

--- src/netserver.h
+++ src/netserver.h
@@ -3,6 +3,22 @@
 #include <boost/asio.hpp>
 #include <vector>
 
+class RWHandle
+{
+public:
+  RWHandle(boost::asio::io_context &ioc) : sock(ioc) {}
+  ~RWHandle()
+  {
+  }
+  boost::asio::ip::tcp::socket &GetSock()
+  {
+    return sock;
+  }
+
+private:
+  boost::asio::ip::tcp::socket sock;
+};
+
 class NetServer
 {
 
@@ -20,20 +36,4 @@ private:
   int _port;
   int _maxcon;
   int _nowcon;
-}
-
-class RWHandle
-{
-public:
-  RWHandle(boost::asio::io_context &ioc) : sock(ioc) {}
-  ~RWHandle()
-  {
-  }
-  boost::asio::ip::tcp::socket &GetSock()
-  {
-    return sock;
-  }
-
-private:
-  boost::asio::ip::tcp::socket sock;
-}
+};
