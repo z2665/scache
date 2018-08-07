3a2988ceaa07fbb389c1f88795ccc3fe238a5ac0
z2665 <z26650@hotmail.com>
2018-08-07 23:52:31 +0800

    修复类型错误

--- src/helper.cpp
+++ src/helper.cpp
@@ -9,13 +9,13 @@ std::vector<std::string> SplitString(std::string_view s, std::string_view c)
     pos1 = 0;
     while (std::string::npos != pos2)
     {
-        v.push_back(s.substr(pos1, pos2 - pos1));
+        v.push_back(std::string(s.substr(pos1, pos2 - pos1)));
         pos1 = pos2 + c.size();
         pos2 = s.find(c, pos1);
     }
     if (pos1 != s.length())
     {
-        v.push_back(s.substr(pos1));
+        v.push_back(std::string(s.substr(pos1)));
     }
     return v;
 }
